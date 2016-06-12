d4961389cf22f0474d6400bd12828980c8efdff4
Kessler <kessler.dev@gmail.com>
2016-06-12 15:40:00 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 xbuild /p:Configuration=Release SkimiaOS.sln /p:TargetFrameworkVersion="v4.5" /p:DebugSymbols=False
+mkdir SkimiaOS.CLI.ServerConsole/bin/Release/core/
+cp -R Plugins/SkimiaOS.Plugins.BasePlugin/bin/Release/* SkimiaOS.CLI.ServerConsole/bin/Release/core/
